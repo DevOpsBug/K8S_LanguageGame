8e2a4f7527c286451e010473935cf2c6d1cbcd82
DevOpsBug <devopsbug.com@gmail.com>
2025-06-16 15:49:33 +0000

    added /language-game api to flask

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg
 from config import DB_CONFIG, API_CONFIG, ASSET_CONFIG
+import random
+
 app = Flask(__name__)
 
 @app.route("/assets/all", methods=["GET"])
@@ -34,9 +36,54 @@ def get_attribution():
                         "license_pdf_url": f"{ASSET_CONFIG['license_url_prefix']}{license_pdf_url}", 
                         "attribution_text": attribution_text, 
                         "attribution_html": attribution_html
-                    })
-                
+                    })                
             return jsonify(return_list)
+        
+@app.route("/language-game", methods=["GET"])
+def get_language_game():
+    language = request.args.get("language")
+    category = request.args.get("category")
+    if not language or not category:
+        return jsonify({"error": "Missing 'language' or 'category' parameter"}), 400
+
+    with psycopg.connect(**DB_CONFIG) as conn:
+        with conn.cursor() as cur:
+            cur.execute(f"""
+    SELECT asset_name, asset_category, {language}_word, image_filename, file_type, 
+        image_width, image_height, {language}_audio_filename 
+    FROM image_assets 
+    WHERE asset_category = '{category}';
+    """)
+            rows = list(cur.fetchall())
+            print(f"Fetched {len(rows)} rows for category '{category}'")
+            random.shuffle(rows)
+            random_rows = rows[:4]
+            random_rows_json = list()
+            for row in random_rows:
+                (asset_name, asset_category, word, image_filename, file_type, 
+        image_width, image_height, audio_filename) = row
+                row_json = {
+                    "asset_name": asset_name, 
+                    "asset_category": asset_category, 
+                    "word": word, 
+                    "image_filename": f"{ASSET_CONFIG['image_url_prefix']}{asset_category}/{image_filename}",
+                    "file_type": file_type, 
+                    "image_width": image_width, 
+                    "image_height": image_height, 
+                    "audio_filename": f"{ASSET_CONFIG['audio_url_prefix']}{asset_category}/{audio_filename}"
+                }
+                random_rows_json.append(row_json)
+            correct_element = random_rows_json[0]
+            wrong_elements = random_rows_json[1:]
+            return jsonify(
+                {
+                    "category": category,
+                    "correctImage": correct_element['image_filename'],
+                    "incorrectImages": [x['image_filename'] for x in wrong_elements],
+                    "word": correct_element['word'],
+                    "audioUrl": correct_element['audio_filename']
+                }
+                )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=API_CONFIG["port"])
